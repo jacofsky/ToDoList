6e04136709a46b29bb791b91657742ba8a52c161
jaco <valentinjacofskyort@gmail.com>
2021-08-21 22:45:45 -0300

    fix timer

--- js/timer.js
+++ js/timer.js
@@ -2,38 +2,46 @@
 // let botonSumarMinuto = document.getElementById('masUnMinuto');
 
 let empezar = document.getElementById('empezarTimer');
-
+let contando = false;
 
 
 empezar.onclick = () => {
+    
+    
+    if (contando == false) {
     let pMinutos = document.getElementById('contenedorRelog');
     let tiemtiempopoParaAlarma = 25;
     tiempo = tiemtiempopoParaAlarma * 60
-    temporizador(tiempo, pMinutos);
+    temporizadorFunction(tiempo, pMinutos);
+    contando = true;
+    }
 }
 
 
 
-function temporizador (tiempo, pMinutos) {
-let temporizador = setInterval(() => {
+
+function temporizadorFunction (tiempo, pMinutos) {
     
-    let minutos = Math.floor(tiempo / 60);
-    let segundos = tiempo % 60;
+    let temporizador = setInterval(() => {
     
-    if (segundos < 10){
+        let minutos = Math.floor(tiempo / 60);
+        let segundos = tiempo % 60;
+    
+        if (segundos < 10){
         pMinutos.innerHTML = `${minutos}:0${segundos}`;
-    } else {
+        } else {
         pMinutos.innerHTML = `${minutos}:${segundos}`;
-    }
+        }
 
-    if (minutos == 0 && segundos == 0) {
-        clearInterval(temporizador);
-        console.log('RINGGGG!!')
-    }
-    tiempo --;
-}, 1000);
-
+        if (minutos == 0 && segundos == 0) {
+            clearInterval(temporizador);
+            console.log('RINGGGG!!')
+        }
+        tiempo --;
+    }, 1000);
+    
 }
+
 
 function restarMinuto () {
     minuto--;
