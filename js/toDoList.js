e178504f2f7341fb750a049f75be39c1cf4fb26a
jaco <valentinjacofskyort@gmail.com>
2021-08-22 10:51:26 -0300

    add storageJson

--- js/toDoList.js
+++ js/toDoList.js
@@ -58,19 +58,62 @@ function datosForm(e) {
     }
 
     if (validarTarea(tarea.value) && validarMasInfo(masInfo.value) && validarDia(dia.value)) {
-        let tareaNueva = new GuardarTareas(tarea.value, masInfo.value, dia.value);
-        tareasAlmacenadas.push(tareaNueva);
+        let tareaNueva = new GuardarTareas(tarea.value, masInfo.value, dia.value, true);
+        
         tareaNueva.agregaryQuitarEnTareas();
         tareaNueva.agregarEnHistorial();
+        tareasAlmacenadas.push(tareaNueva);
+        guardarTareaEnStorage(tareasAlmacenadas)
         modal.style.display = "none";
+        
     }
 }
+
+// 5. Guardar tarea en storage 
+
+function guardarTareaEnStorage (tareasAlmacenadas) {
+    localStorage.setItem("TareaAlmacenada", JSON.stringify(tareasAlmacenadas));
+}
+
+// 6. Obtener el storage
+
+function obtenerElStorage (tareasAlmacenadas) {
+
+    itemsAlmacenados = JSON.parse(localStorage.getItem("TareaAlmacenada"))
+    for (let i = 0; i < itemsAlmacenados.length; i++) {
+        tareasAlmacenadas.push(new GuardarTareas(itemsAlmacenados[i].tarea, itemsAlmacenados[i].masInfo, itemsAlmacenados[i].dia, itemsAlmacenados[i].activo))
+    }
+    iterarStorageEnElDom(tareasAlmacenadas)
+}
+
+// 7. Iterar en el storage en el DOM
+
+function iterarStorageEnElDom () {
+    for (let i = 0; i < tareasAlmacenadas.length; i++) {
+        if (tareasAlmacenadas[i].activo === true){
+            tareasAlmacenadas[i].agregaryQuitarEnTareas();
+            tareasAlmacenadas[i].agregarEnHistorial();
+        } else {
+            tareasAlmacenadas[i].agregarEnHistorial();
+        }
+    }
+}
+
+// 8. Actualizar Storage En el
+
+function actualizarStorage () {
+    localStorage.removeItem("TareaAlmacenada");
+    guardarTareaEnStorage(tareasAlmacenadas);
+}
+
 
 
 const FECHA = new Date();
 const tareasAlmacenadas = []
 const contenedorDeTareas = document.getElementById('listaTareas');
 const contenedorDeTareasHistorial = document.getElementById('contenedorDeTareasEnHistorial')
+obtenerElStorage(tareasAlmacenadas)
+
 
 let formulario = document.getElementById('formularioTarea')
 formulario.addEventListener("submit", datosForm)
@@ -78,29 +121,32 @@ formulario.addEventListener("submit", datosForm)
 
 
 
-
-
-
 class GuardarTareas {
-    constructor(tarea, masInfo, dia) {
+    constructor(tarea, masInfo, dia, activo) {
         this.tarea = tarea;
         this.masInfo = masInfo;
         this.dia = dia;
+        this.activo = activo;
     }
 
     agregaryQuitarEnTareas() {
         let liAlmacenaTarea = document.createElement("li");
+        let botonBorrarTarea = document.getElementById("borrarTarea");
+
+        
         liAlmacenaTarea.setAttribute("class", "taskCard p-1 my-3 d-flex justify-content-between")
     
         liAlmacenaTarea.innerHTML = `
                 <p class="taskCard__text my-auto">${this.tarea}</p>
-                <button class="trashButton">
+                <button class="trashButton" id="borrarTarea">
                 <i class="bi bi-trash"></i>
                 </button>
             `
 
         liAlmacenaTarea.onclick = () => {
             liAlmacenaTarea.remove();
+            this.activo = false;
+            actualizarStorage();
         }
         contenedorDeTareas.appendChild(liAlmacenaTarea)
     }
@@ -111,10 +157,6 @@ class GuardarTareas {
         liAlmacenaTarea.innerHTML = ` <p class="taskCard__text my-auto">${this.tarea}</p> `
         contenedorDeTareasHistorial.appendChild(liAlmacenaTarea)
     }
-    
-    subirTareaStorage() {
-
-    }
-
 }
+
 
